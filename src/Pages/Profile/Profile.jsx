0492284b191b69bd7449190355a5f8a941ad14cb
fahmida-oni2 <onifahmida@gmail.com>
2025-10-23 22:53:09 +0600

    added the errorpage

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import userImg from '../../assets/user.png'
 const Profile = () => {
     return (
-        <div>
-            profile
-        </div>
+       <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row">
+    <img
+      src={userImg}
+      className="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 className="text-3xl font-bold">Fahmida Rahman</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     );
 };
 
